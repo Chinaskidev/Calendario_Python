da083602889553d12a35b830042a2cd688c9e236
Chinaskidev <chinaskicharles0@gmail.com>
2023-10-17 18:50:09 -0300

    segundo cambio

--- calendario.py
+++ calendario.py
@@ -2,52 +2,62 @@ import calendar
 import tkinter as tk
 import datetime
 
+# Obtenemos los valores del año y mes a mostrar
 año = datetime.date.today().year
 mes = datetime.date.today().month
-year = año
-month = mes
-
-def Calendario(year, month):
-    str1 = calendar.month(year, month)
-    label1.configure(text=str1)
-
+year= año
+month= mes
+ 
+def writeCalendar(year,month):
+	# Asignamos el año y mes al calendario
+	str1 = calendar.month(year, month)
+ 
+	label1.configure(text=str1)
+ 
 def mesAnterior():
-    global month, year
-    month -= 1
-    if month == 0:
-        month = 12
-        year -= 1
-    Calendario(year, month)
-
+	global month,year
+	month-=1
+	if month==0:
+		month=12
+		year-=1
+ 
+	writeCalendar(year,month)
+ 
 def mesSiguiente():
-    global month, year
-    month += 1
-    if month == 13:
-        month = 1
-        year += 1
-    Calendario(year, month)
+	global month,year
+	month+=1
+	if month==13:
+		month=1
+		year+=1
+ 
+	writeCalendar(year,month)
+ 
+def on_enter(event):
+    event.widget.config(bg="lightblue")
 
-def mostrar_fecha():
-    fecha = f"{month}/{year}"  # Formato de fecha (puedes ajustarlo según tus preferencias)
-    entry_fecha.delete(0, tk.END)  # Borra el contenido actual del Entry
-    entry_fecha.insert(0, fecha)  # Inserta la nueva fecha
+def on_leave(event):
+    event.widget.config(bg="white")
 
+ 
 root = tk.Tk()
 root.title("Calendario")
-
-label1 = tk.Label(root, text="", font=('courier', 14, 'bold'), bg='white', justify=tk.LEFT)
-label1.grid(row=1, column=1)
-
-frame = tk.Frame(root, bd=5)
-anterior = tk.Button(frame, text="Anterior", command=mesAnterior)
+ 
+# Lo posicionamos en un label
+label1 = tk.Label(root, text="", font=('courier', 30, 'bold'), bg='white', justify=tk.LEFT)
+label1.grid(row=1,column=1)
+ 
+# ponemos los botones dentro un Frame
+frame=tk.Frame(root,bd=5)
+anterior = tk.Button(frame,text="Anterior", command=mesAnterior)
 anterior.grid(row=1, column=1, sticky=tk.W)
-siguiente = tk.Button(frame, text="Siguiente", command=mesSiguiente)
+siguiente = tk.Button(frame,text="Siguiente", command=mesSiguiente)
 siguiente.grid(row=1, column=2)
-frame.grid(row=2, column=1)
+frame.grid(row=2,column=1)
+ 
+writeCalendar(year,month)
+label1.bind("<Enter>", on_enter)
+label1.bind("<Leave>", on_leave)
 
-entry_fecha = tk.Entry(root, font=('courier', 14, 'bold'))
-entry_fecha.grid(row=3, column=1)
-
-mostrar_fecha()
-
+ 
+# ejecutamos el evento loop
 root.mainloop()
